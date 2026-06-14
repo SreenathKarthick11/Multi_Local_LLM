628c84ff7dece4561fb1597c69031c4746ea3273
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-14 21:41:32 +0530

    Phase 3: completed the basic debate Model.

--- app.py
+++ app.py
@@ -1,43 +1,16 @@
-from typing import TypedDict
-from langchain_ollama import ChatOllama
-from langgraph.graph import StateGraph,START, END
+from graph import graph
+
+initial_state = {
+    "question": input("Question: "),
+    "answer_a": "",
+    "answer_b": "",
+    "final_answer": ""
+}
 
 
-class DebateState(TypedDict):
-    question: str
-    answer: str
+# print(graph.get_graph().draw_ascii())
 
-
-llm = ChatOllama(
-    model="qwen2.5:3b",
-    temperature=0.7
-)
-
-
-def answer_node(state: DebateState) -> DebateState:
-    response = llm.invoke(state["question"])
-    state["answer"] = response.content
-
-    return state
-
-
-graph_builder = StateGraph(DebateState)
-
-graph_builder.add_node("answer_node",answer_node)
-
-graph_builder.add_edge(START,"answer_node")
-
-graph_builder.add_edge("answer_node",END)
-
-graph = graph_builder.compile()
-
-
-result = graph.invoke(
-    {
-        "question": "What is the capital of France?"
-    }
-)
+result = graph.invoke(initial_state)
+print("\n===== FINAL STATE =====\n")
 
 print(result)
-
-print(graph.get_graph().draw_ascii())
