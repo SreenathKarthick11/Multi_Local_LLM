199f0ed959cee9f0d6159e466752547467d4ae01
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-15 18:37:12 +0530

    Phase 4: update to use the structed response model.

--- llm.py
+++ llm.py
@@ -1,6 +1,9 @@
 from langchain_ollama import ChatOllama
+from models import AgentResponse
 
 llm = ChatOllama(
     model="qwen2.5:3b",
     temperature=0.7
 )
+
+structured_llm = llm.with_structured_output(AgentResponse)
--- state.py
+++ state.py
@@ -1,10 +1,12 @@
 from typing import TypedDict
 
+from models import AgentResponse
+
 
 class DebateState(TypedDict):
     question: str
 
-    answer_a: str
-    answer_b: str
+    answer_a: AgentResponse | None
+    answer_b: AgentResponse | None
 
     final_answer: str
