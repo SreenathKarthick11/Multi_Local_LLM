dec5af85e42c7f6f95f051354a478cd94838ceb6
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-18 21:42:43 +0530

    Phase 5 : Updated the critque response

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 from typing import TypedDict
 
-from models import AgentResponse
+from models import AgentResponse, CritiqueResponse
 
 
 class DebateState(TypedDict):
@@ -9,8 +9,8 @@ class DebateState(TypedDict):
     answer_a: AgentResponse | None
     answer_b: AgentResponse | None
 
-    critique_a: str
-    critique_b: str
+    critique_a: CritiqueResponse | None
+    critique_b: CritiqueResponse | None
 
     revised_answer_a: AgentResponse | None
     revised_answer_b: AgentResponse | None
