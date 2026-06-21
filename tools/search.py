774af7403cb1ab8cf3b05cd55ea4d42de90fa6c3
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-21 20:55:38 +0530

    Phase 7 : Adding Websearch tool

---
+++ tools/search.py
@@ -0,0 +1,14 @@
+from duckduckgo_search import DDGS
+
+def search_web(query: str, max_results: int = 5) -> str:
+
+    results = []
+
+    with DDGS() as ddgs:
+        for r in ddgs.text(query, max_results=max_results):
+            results.append(
+                f"Title: {r['title']}\n"
+                f"Body: {r['body']}\n"
+            )
+
+    return "\n\n".join(results)
