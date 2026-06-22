55b861e4942227903c2d135eeabaab4bfae7abb4
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-22 21:54:05 +0530

    Phase 8: Multi Round Debate, hence updated state to have history.

--- state.py
+++ state.py
@@ -1,21 +1,27 @@
-from typing import TypedDict
-
+from operator import add
+from typing import TypedDict,Annotated
 from models import AgentResponse, CritiqueResponse
 
 
 class DebateState(TypedDict):
     question: str
 
+    round_number: int
+    max_rounds: int
+
     evidence_a: str
     evidence_b: str
 
-    answer_a: AgentResponse | None
-    answer_b: AgentResponse | None
+    # answer_a: AgentResponse | None
+    # answer_b: AgentResponse | None
 
     critique_a: CritiqueResponse | None
     critique_b: CritiqueResponse | None
 
-    revised_answer_a: AgentResponse | None
-    revised_answer_b: AgentResponse | None
+    # revised_answer_a: AgentResponse | None
+    # revised_answer_b: AgentResponse | None
+
+    history_a: Annotated[list[AgentResponse],add]
+    history_b: Annotated[list[AgentResponse],add]
 
     final_answer: str
