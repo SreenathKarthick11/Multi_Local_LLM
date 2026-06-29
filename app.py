287e158892d0b755cf03a57cf96a3d39f1b441e4
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-06-29 17:46:56 +0530

    Phase 15: Completed incoperating RAG.

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
-from graph import graph
 from pprint import pprint
 
-initial_state = {
-    "question": input("Question: "),
-
-    "round_number": 1,
-    "max_rounds": 3,
-
-    "search_used_a": False,
-    "search_used_b": False,
-
-    "evidence_a": "",
-    "evidence_b": "",
-
-    "critique_a": None,
-    "critique_b": None,
-
-    "final_answer": ""
-}
+from graph import graph
 
 
-# print(graph.get_graph().draw_ascii())
+def main():
 
-result = graph.invoke(initial_state)
-print("\n===== FINAL STATE =====\n")
-pprint(result)
+    initial_state = {
+        "question": input("Question: "),
+
+        "round_number": 1,
+        "max_rounds": 3,
+
+        "search_used_a": False,
+        "search_used_b": False,
+
+        "resource_bank_a": [],
+        "resource_bank_b": [],
+
+        "history_a": [],
+        "history_b": [],
+
+        "critique_a": None,
+        "critique_b": None,
+
+        "judge_result": None,
+
+        "stop_reason": None,
+    }
+
+    # Uncomment to visualize the graph
+    # print(graph.get_graph().draw_ascii())
+
+    result = graph.invoke(initial_state)
+
+    print("\n===== FINAL STATE =====\n")
+    pprint(result)
+
+
+if __name__ == "__main__":
+    main()
